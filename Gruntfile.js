cd4fcd9938481929e89089620120602e703e3e6c
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-31 15:20:11 -0500

    folder structure

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+//this is a config file
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'), 
+        uglify: {
+            build: {
+                src: 'client/script/client.js',
+                dest: 'server/public/script/client.mn.js'
+            }
+        },
+        copy: {
+            jquery: {
+                expand: true,
+                cwd: 'node_modules/jquery/dist/',
+                src: ['jquery.js'],
+                dest: 'server/public/vendors/'
+            },
+            bootstrap: {
+                expand: true,
+                cwd: 'node_modules/bootstrap/dist/css/',
+                src: ['bootstrap.css'],
+                dest: 'server/public/vendors/'
+            },
+            html: {
+                expand: true,
+                cwd: 'client/',
+                src: ['index.html'],
+                dest: 'server/public/'
+            },
+            css: {
+                expand: true,
+                cwd: 'client/styles/',
+                src: ['style.css'],
+                dest: 'server/public/styles/'
+            },
+
+        },
+        watch: {
+            files: ['client/script/*.js', 'client/views/*.html', 'client/styles/*.css'],
+            tasks: ['uglify', 'copy']
+        }
+
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy'); //loads in grunt-contrib-copy module - next function will actually use this copy
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    grunt.registerTask('default', ['uglify', 'copy', 'watch']); //describe different tasks to do - set up task as default. [copy] can be named anything here.
+};
---
+++ server/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require("path");
+var mongoose = require('mongoose');
+
+
+var mongoURI = "mongodb://herokuintro:intro@ds147920.mlab.com:47920/mongodtbs";
+var MongoDB = mongoose.connect(mongoURI).connection;
+
+MongoDB.on("error", function(err) {
+  console.log("Mongo Connection Error: " + err);
+});
+
+MongoDB.once("open", function() {
+  console.log('Woot Woot! Connected to Mongo');
+});
+
+
+app.set("port", (process.env.PORT || 5000));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static('server/public'));
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve("server/public/views/index.html"));
+});
+
+app.listen(app.get("port"), function(){
+  console.log("listening on port", app.get("port"));
+});
