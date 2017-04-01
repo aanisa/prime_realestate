8c8b68295a3ec3d85648910d25136308964debe2
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-04-01 14:20:33 -0500

    empty options and inputs when submit

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -3,38 +3,6 @@ $(document).ready(function() {
 
     getListings();
 
-    // $('.listingTypeSearch').on('change', function() {
-    //   console.log('search house');
-    //   var value = $(this).val();
-    //   alert(value);
-    // });
-
-    $('.addListing').on('submit', function(event) {
-        event.preventDefault();
-        console.log($('#valueOfProperty').val());
-        console.log($('#sqftOfProperty').val());
-        console.log($('#cityOfProperty').val());
-        console.log($('#listingTypeAdd').val());
-
-        var listingType = $('#listingTypeAdd').val();
-
-        if (listingType === 'House') {
-            console.log('House Added');
-        }
-
-        if (listingType === 'Apartment') {
-            console.log('Apartment Added');
-        }
-    });//
-
-
-    $('.searchListings').on('submit',function() {
-      event.preventDefault();
-      console.log($('#listingTypeSearch').val());
-    });
-
-
-
 
     // function addHouseListing() {
     //   $.ajax({
@@ -60,6 +28,45 @@ $(document).ready(function() {
     // }
 
 
+    // function addListingbyType() {
+        $('.addListing').on('submit', function(event) {
+            event.preventDefault();
+            console.log($('#valueOfProperty').val());
+            console.log($('#sqftOfProperty').val());
+            console.log($('#cityOfProperty').val());
+            console.log($('#listingTypeAdd').val());
+
+            var listingType = $('#listingTypeAdd').val();
+
+            if (listingType === 'House') {
+                console.log('House Added');
+            }
+
+            if (listingType === 'Apartment') {
+                console.log('Apartment Added');
+            }
+            //empty input value and select option
+            $('#valueOfProperty').val('');
+            $('#sqftOfProperty').val('');
+            $('#cityOfProperty').val('');
+            $('#listingTypeAdd').val('');
+        }); //
+
+    // } //
+
+
+    // function searchListingsbyType() {
+        $('.searchListings').on('submit', function() {
+            event.preventDefault();
+            console.log($('#listingTypeSearch').val());
+
+            //empty select option
+            $('#listingTypeSearch').val('');
+        });
+    // } //
+
+
+
 }); //doc ready
 
 
@@ -81,11 +88,13 @@ function getListings() {
 function appendListings(array) {
     for (var i = 0; i < array.length; i++) {
         var newArray = array[i];
+
         var $hl = $('.houseListings').append();
         var $al = $('.apartmentListings').append();
+        //append listings in two different containers depinding on type
         if (newArray.rent === undefined) {
             $hl.append('<div class="col-md-2 newListing ">' +
-                '<img src="imgs/home.png" alt="house" class="img-responsive">'+
+                '<img src="imgs/home.png" alt="house" class="img-responsive">' +
                 '<h4>' + newArray.city + '</h4>' +
                 '<p>' + newArray.sqft + ' sq.ft</p>' +
                 '<p>For Sale: $' + newArray.cost + '</p>' + '</div>');
@@ -93,7 +102,7 @@ function appendListings(array) {
         } //if rent is undefined because it's a house, append listing
         if (newArray.cost === undefined) {
             $al.append('<div class="col-md-2 newListing">' +
-                '<img src="imgs/building.png" alt="building" class="img-responsive">'+
+                '<img src="imgs/building.png" alt="building" class="img-responsive">' +
                 '<h4>' + newArray.city + '</h4>' +
                 '<p>' + newArray.sqft + ' sq.ft</p>' +
                 '<p>For Rent: $' + newArray.rent + '</p>' + '</div>');
